ea43bdbd3af005184b2d7b55464de25a0bb021e6
satheesh-test <sandeeppromods@gmail.com>
2021-05-04 19:35:56 +0530

    Simple changes

--- DailyTests/Test_pytest.py
+++ DailyTests/Test_pytest.py
@@ -3,12 +3,11 @@ from selenium import webdriver
 import time
 from selenium.webdriver.support.select import Select
 
-global driver
 
-
-@pytest.fixture()
-def setUp(self):
-    driver = webdriver.Chrome(executable_path="D:\softwares\Chrome Driver\chromedriver.exe")
+@pytest.fixture(scope='module')
+def setUp():
+    global driver
+    driver = webdriver.Chrome(executable_path="D://softwares//Chrome Driver//chromedriver.exe")
     driver.get("https://letskodeit.teachable.com/p/practice")
     driver.maximize_window()
     time.sleep(5)
