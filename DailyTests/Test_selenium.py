e0c8ac22cb78cd7644f0270f8713adb5d3988ed8
satheesh-test <sandeeppromods@gmail.com>
2021-05-05 13:45:27 +0530

    Second Branch

--- DailyTests/Test_selenium.py
+++ DailyTests/Test_selenium.py
@@ -104,7 +104,6 @@ def testSwitchingAlert(setUp):
 # Scrolling down the web page
 def testScrollingDown(setUp):
     driver.execute_script("window.scrollBy(0, 1000000);")
-    time.sleep(3)
 
 
 # Scrolling up the web page
